7e6c60187bc5cc55ea598d36b05228a88ea9fc59
Chase Bowden <chasebowden@Chases-MacBook-Pro.local>
2018-04-06 15:57:38 -0700

    fix api generator bug :(

--- src/app/models/source.model.ts
+++ src/app/models/source.model.ts
@@ -1,6 +1,6 @@
 export class Source {
   uri: Object;
   constructor (public name: string, public url: string, public description: string, public categories: string[]) {
-    this.uri = {"sourceURi":this.url};
+    this.uri = {"sourceUri":this.url};
   }
 }
