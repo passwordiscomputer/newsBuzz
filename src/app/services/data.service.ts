f14b30b2f635a6d5d14958099d2607fb37243f97
Chase Bowden <chasebowden@Chases-MacBook-Pro.local>
2018-04-06 11:50:51 -0700

    continue experimenting with api

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,44 +1,22 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
+import { HttpParams } from '@angular/common/http';
 
 @Injectable()
 export class DataService {
 
   constructor(private http: HttpClient) { }
-  url = "http://eventregistry.org/json/article";
 
-  params = {
-    "query": {
-      "$query": {
-        "$and": [{
-          "keyword": {
-            "$and": [
-              "Politics",
-              "Mad"
-            ]
-          }
-        },
-        {
-          "categoryUri": {
-            "$and": [
-              "dmoz/Arts/Entertainment"
-            ]
-          }
-        }
-        ]
-      },
-      "$filter": {}
-    },
-    "action": "getArticles",
-    "resultType": "articles",
-    "articlesSortBy": "date",
-    "articlesCount": 100,
-    "articlesArticleBodyLen": -1
-  }
+  params = { "$query": { "$and": [{ "categoryUri": { "$and": ["dmoz/Society/Politics"] } }, { "$or": [{ "sourceUri": "bloomberg.com" }, { "sourceUri": "washingtonpost.com" }] }] }, "$filter": {} };
+  url = `http://eventregistry.org/json/article?query=${JSON.stringify(this.params)}&action=getArticles&resultType=articles&articlesSortBy=date&articlesCount=10&articlesArticleBodyLen=-1&apiKey=e7b30769-23df-462b-8ee1-440aa0784c21`;
+
+
 
   getData() {
+    this.params.$query.$and[1].$or.push({"sourceUri": "bloomberg"});
+    console.log(this.params.$query.$and[1].$or);
     this.http.get(this.url).subscribe(res => {
-      console.log(JSON.stringify(this.params));
+      console.log(res);
     });
   }
 }
